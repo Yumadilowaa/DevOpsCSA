15e090368212465195e3bf612e0db48ef9eec44f
yumadilowaa <ms.yumadilowa@mail.ru>
2022-06-26 10:37:50 +0300

    hw

---
+++ hw2.py
@@ -0,0 +1,11 @@
+
+print("Enter a line:")
+line_origin = input()
+words = line_origin.split()
+def get_unique(line):
+    unique = []
+    for u in line:
+        if u not in unique:
+            unique.append(u)
+    print(" ".join(unique))
+get_unique(words)
