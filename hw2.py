1b3966695fd272a9fe0048e351b9d976605d246b
yumadilowaa <ms.yumadilowa@mail.ru>
2022-06-26 12:35:30 +0300

    modified:   hw2.py

--- hw2.py
+++ hw2.py
@@ -1,11 +1,10 @@
-
-print("Enter a line:")
-line_origin = input('-> ')
-words = line_origin.split()
 def get_unique(line):
     unique = []
     for u in line:
         if u not in unique:
             unique.append(u)
     print(" ".join(unique))
+print("Enter a line:")
+line_origin = input('-> ')
+words = line_origin.split()
 get_unique(words)
